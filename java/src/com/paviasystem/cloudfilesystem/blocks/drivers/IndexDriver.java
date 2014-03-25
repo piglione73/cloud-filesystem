3e1606cecc67943d9760af032f0492324930150a
Roberto Rossi <roberto.rossi@paviasystem.com>
2014-03-25 18:49:38 +0100

    Development

--- java/src/com/paviasystem/cloudfilesystem/blocks/drivers/IndexDriver.java
+++ java/src/com/paviasystem/cloudfilesystem/blocks/drivers/IndexDriver.java
@@ -1,6 +1,5 @@
 package com.paviasystem.cloudfilesystem.blocks.drivers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -13,56 +12,54 @@ public class IndexDriver implements Index {
 	public IndexDriver(Index index) {
 		this.index = index;
 	}
-	
+
 	public LinkedList<LogBlobIndexEntry> readLogBlobEntries(String fileBlobName, long lsn1, String randomId1, long lsn2, String randomId2) {
 		//First, read all log blob index entries >= lsn1 and <= lsn2 and arrange them in a two-level map, for subsequent faster lookups
 		HashMap<Long, HashMap<String, LogBlobIndexEntry>> entries = new HashMap<>();
-		for(LogBlobIndexEntry entry:index.readLogBlobEntries(fileBlobName, lsn1, lsn2)){
+		for (LogBlobIndexEntry entry : index.readLogBlobEntries(fileBlobName, lsn1, lsn2)) {
 			HashMap<String, LogBlobIndexEntry> map = entries.get(entry.logBlobLsn);
-			if(map == null) {
+			if (map == null) {
 				map = new HashMap<>();
 				entries.put(entry.logBlobLsn, map);
 			}
-			
+
 			map.put(entry.logBlobRandomId, entry);
 		}
-		
-		/*Then start from lsn2/randomId2 and proceed backwards up to lsn1/randomId1, removing all log entries that are not in the chain.
+
+		/*
+		 * Then start from lsn2/randomId2 and proceed backwards up to
+		 * lsn1/randomId1, removing all log entries that are not in the chain.
 		 */
 		LinkedList<LogBlobIndexEntry> chain = new LinkedList<>();
 		long lsn = lsn2;
 		String id = randomId2;
-		while() {
-			
-		}
-		LinkedList<LogBlobIndexEntry> entries = new LinkedList<LogBlobIndexEntry>();
+		while (lsn >= lsn1) {
+			//Find lsn/id
+			HashMap<String, LogBlobIndexEntry> randomIds = entries.get(lsn);
+			if (randomIds != null) {
+				//LSN found
+				LogBlobIndexEntry entry = randomIds.get(id);
+				if (entry != null) {
+					//Entry found: add to chain...
+					chain.addFirst(entry);
 
-		//Read backwards up to and excluding logBlobNameFrom
-		String nextLogBlobNameToRead = logBlobNameTo;
-		while (true) {
-			LogBlobIndexEntry logBlobEntry = index.readLogBlobEntry(nextLogBlobNameToRead);
-			if (logBlobEntry == null) {
-				//If not found, stop
-				break;
-			}
-
-			//If found, store
-			entries.addFirst(logBlobEntry);
-
-			//Then prepare to read the subsequent element
-			nextLogBlobNameToRead = logBlobEntry.previousLogBlobName;
-
-			//If we reached the end, stop
-			if (nextLogBlobNameToRead == null || nextLogBlobNameToRead.trim().isEmpty()) {
-				//Reached the first log entry, so we cannot proceed any further
-				break;
-			} else if (nextLogBlobNameToRead.equals(logBlobNameFrom1) || nextLogBlobNameToRead.equals(logBlobNameFrom2)) {
-				//Reached logBlobNameFrom1/2, so we are required to stop
+					//...and prepare to move to previous entry
+					lsn--;
+					id = entry.previousLogBlobRandomId;
+				} else {
+					//Entry not found: end
+					break;
+				}
+			} else {
+				//LSN not found: end
 				break;
 			}
 		}
 
-		return entries;
+		/*
+		 * Now we have the chain in LSN order. Let's return it
+		 */
+		return chain;
 	}
 
 }
